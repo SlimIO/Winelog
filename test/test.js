b3cbb6366a8539531895e0a2fc25068b7cb6c666
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2020-02-07 06:11:36 +0100

    test: add bare min test

--- test/test.js
+++ test/test.js
@@ -2,7 +2,29 @@
 
 // Require Third-party Dependencies
 const avaTest = require("ava");
+const is = require("@slimio/is");
 
-avaTest("Husky passed Test", (assert) => {
-    assert.pass();
+// Require Internal Dependencies
+const winServices = require("../");
+
+avaTest("winServices must export a files constants", (assert) => {
+    const hasFiles = Reflect.has(winServices, "files");
+    assert.true(hasFiles);
+    assert.true(Object.isFrozen(winServices.files));
+    assert.deepEqual(Object.keys(winServices.files).sort(), [
+        "Application", "Security", "System", "DirectoryService", "DNSServer", "FileReplicationService"
+    ].sort());
+});
+
+avaTest("winServices.readEventLog must be an async iterable that return event log Objects", async(assert) => {
+    const asyncIterator = winServices.readEventLog(winServices.files.Security);
+    assert.true(is.asyncIterable(asyncIterator));
+
+    for await (const event of asyncIterator) {
+        assert.true(is.plainObject(event));
+        assert.is(typeof event.eventId, "number");
+        assert.is(typeof event.providerName, "string");
+        assert.is(typeof event.providerGUID, "string");
+        break;
+    }
 });
